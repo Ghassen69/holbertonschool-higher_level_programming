a0c7990f6e801eaaeb8f864ca5ca817fc74cea0d
Ghassen69 <ghassenayai2000@gmail.com>
2021-10-06 06:27:54 -0700

    get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv;
+const URL = args[2];
+const request = require('request');
+request(URL, function (err, response) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('code:', response.statusCode);
+});
