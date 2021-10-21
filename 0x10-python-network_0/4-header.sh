a1d1a92e9869a07b04f323739d02c0d3e2609146
Ghassen69 <ghassenayai2000@gmail.com>
2021-10-21 02:08:19 -0700

    4 fixed

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends GET request with custom header and displays the response body
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Displays the body of the response of a curl request with header
+curl -sH "X-HolbertonSchool-User-Id:98" "$1"
