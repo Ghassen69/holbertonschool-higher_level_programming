109c8a9bb2ccb61435b6dca878cc3b74181dfeb8
Ghassen69 <ghassenayari2000@gmail.com>
2022-12-23 14:18:18 -0200

    4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const req = require('request');
+const url = process.argv[2]
+req.get(url, (error, res, body) => {
+  if (error) {
+    console.log(err);
+  }
+  else {
+    let count = 0;
+    const films = JSON.parse(body).results;
+    films.forEach(film => {
+      film.characters.forEach(character => {
+        if (character.includes('people/18/')) {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
