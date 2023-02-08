e3a17b15fa411b6d08bf5c7ad68a486a7ef91223
Gaston69 <ghassenayari2000@gmail.com>
2023-02-08 12:34:51 -0200

    zeze

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  function convert (x) {
-    return x.toString(base);
-  }
-  return convert;
-};
+const data_list = require('./100-data').list;
+let index = 0;
+const my_list = data_list.map(m => m * i++);
+console.log(my_list).
--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+
 const data_list = require('./100-data').list;
 console.log(data_list);
+
+
 let i = 0;
-const my_list = data_list.map(m => m * i++);
-console.log(my_list);
+const res = data_list.map(m => m * i++);
+console.log(res);
