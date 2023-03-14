72b33049e09e111e7a239df6167bf102e555d104
Ghassen69 <ghassenayari2000@gmail.com>
2023-03-14 12:03:44 -0700

    fx semistand

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -4,28 +4,28 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, function(error, response, body) {
+request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
     const movie = JSON.parse(body);
     const characterUrls = movie.characters;
     const characters = new Array(characterUrls.length).fill(null);
-    
-    const printCharacters = function() {
-      characters.forEach(function(character) {
+
+    const printCharacters = function () {
+      characters.forEach(function (character) {
         console.log(character.name);
       });
     };
-    
-    characterUrls.forEach(function(characterUrl, index) {
-      request(characterUrl, function(error, response, body) {
+
+    characterUrls.forEach(function (characterUrl, index) {
+      request(characterUrl, function (error, response, body) {
         if (error) {
           console.error(error);
         } else {
           const character = JSON.parse(body);
           characters[index] = character;
-          
+
           if (!characters.includes(null)) {
             printCharacters();
           }
