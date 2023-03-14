7dc32d1ddb629a98e10936ac05e754a7fcf2cc74
Ghassen69 <ghassenayari2000@gmail.com>
2023-03-14 10:49:39 -0700

    js appi

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function(error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach(function(todo) {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasksByUser);
+  }
+});
+
