eb2e8a7d571e5355e51c53e6f732a04207d7a3e7
Ghassen69 <ghassenayari2000@gmail.com>
2023-03-15 10:05:17 -0700

    fixng

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Fetches the ID of a GitHub user using their authentication credentials.
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get("https://api.github.com/user", auth=(username, password))
+    json_response = response.json()
+    try:
+        print(json_response['id'])
+    except KeyError:
+        print('None')
