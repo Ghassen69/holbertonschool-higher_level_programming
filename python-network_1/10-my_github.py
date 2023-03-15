d2dac6dbed44924fa4faef6f6abe7274538235e3
Ghassen69 <ghassenayari2000@gmail.com>
2023-03-15 10:07:03 -0700

    fixng

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,7 +8,8 @@ import sys
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-    response = requests.get("https://api.github.com/user", auth=(username, password))
+    response = requests.get("https://api.github.com/user",
+                            auth=(username, password))
     json_response = response.json()
     try:
         print(json_response['id'])
